93042016335e305fb911b1c7ffef5c0d268beadd
Jaspe Michael <micaeljaspe@gmail.com>
2025-07-15 19:27:15 +0200

    Basic page for the new collecteur app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:system_theme/system_theme.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+	WidgetsFlutterBinding.ensureInitialized();
+	await windowManager.ensureInitialized();
+	windowManager.waitUntilReadyToShow().then((_) async {
+		await windowManager.setTitle("Collecteur Soft");
+		await windowManager.setTitleBarStyle(TitleBarStyle.normal);
+		await windowManager.setBackgroundColor(Colors.transparent);
+		await windowManager.setMinimumSize(const Size(755, 545));
+		await windowManager.center();
+		await windowManager.show();
+		await windowManager.setSkipTaskbar(false);
+	});
+
+	runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,6 +26,11 @@ class MyApp extends StatelessWidget {
     return FluentApp(
       title: 'Collecteur Soft',
 	  theme: FluentThemeData(
+		  brightness: Brightness.light,
+		  accentColor: SystemTheme.accentColor.accent.toAccentColor()
+	  ),
+	  darkTheme: FluentThemeData(
+		  brightness: Brightness.dark,
 		  accentColor: SystemTheme.accentColor.accent.toAccentColor()
 	  ),
       home: const HomePage(),
@@ -24,9 +44,86 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with WindowListener{
+  int index = 0;
+
+  @override
+  void initState() {
+	windowManager.addListener(this);
+  	super.initState();
+  }
+
+  @override
+  void dispose() {
+	windowManager.removeListener(this);
+  	super.dispose();
+  }
+
+  @override
+  void onWindowClose() async {
+	bool isPreventClose = await windowManager.isPreventClose();
+	if (isPreventClose) {
+		showDialog(context: context, builder: (_) {
+			return ContentDialog(
+				title: const Text("Confirmation"),
+				content: const Text("Voulez-vous fermer l'application?"),
+				actions: [
+					FilledButton(child: const Text("Oui"),
+					onPressed: () {
+						Navigator.pop(context);
+						windowManager.destroy();
+					}),
+					FilledButton(child: const Text("Non"),
+					onPressed: () {
+						Navigator.pop(context);
+					})
+				],
+			);
+		}
+		);
+	}
+  	super.onWindowClose();
+  }
+
+  Widget _Item1() {
+  	return Text("Add");
+  }
+  Widget _Item2() {
+  	return Text("Subtract");
+  }
+
   @override
   Widget build(BuildContext context) {
-	  return SizedBox();
+	  return NavigationView(
+	  	pane: NavigationPane(
+			selected: index,
+			onChanged: (i) => setState(() {
+						  index = i;
+						}),
+			displayMode: PaneDisplayMode.compact,
+			items: [
+				PaneItem(
+					icon: Icon(FluentIcons.document), 
+					title: Text("Mouvements"),
+					body: _Item1()
+				),
+				PaneItem(
+					icon: Icon(FluentIcons.mountain_climbing), 
+					title: Text("Districts et Collines"),
+					body: _Item2()
+				),
+				PaneItem(
+					icon: Icon(FluentIcons.add_field), 
+					title: Text("Autres champs"),
+					body: _Item2()
+				),
+				PaneItem(
+					icon: Icon(FluentIcons.add_friend), 
+					title: Text("Superviseurs"),
+					body: _Item2()
+				),
+			]
+		),
+	  );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import screen_retriever_macos
 import system_theme
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SystemThemePlugin.register(with: registry.registrar(forPlugin: "SystemThemePlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
