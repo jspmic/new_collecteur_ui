97ecfcb6b3566ed775cd162098bd7b3096a271ab
Jaspe Michael <micaeljaspe@gmail.com>
2025-07-15 21:42:36 +0200

    feat: added new models

---
+++ lib/models/options.dart
@@ -0,0 +1,10 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class Option {
+	final String title;
+	final IconData icon;
+	final Widget body;
+	const Option({required this.title,
+				   required this.icon,
+				   required this.body});
+}
