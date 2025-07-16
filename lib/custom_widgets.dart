905b0062d446681d106b64942b531ed0ddaeabd0
Jaspe Michael <micaeljaspe@gmail.com>
2025-07-16 21:29:04 +0200

    added custom widgets and custom functions

---
+++ lib/custom_widgets.dart
@@ -0,0 +1,17 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:system_theme/system_theme.dart';
+
+// Function to format a date
+String? formatDate(DateTime? _date){
+  if (_date == null){
+    return null;
+  }
+  return "${_date?.day} / ${_date?.month} / ${_date?.year}";
+}
+
+void getDate(BuildContext context, {required Function(DateTime?) onSelect}) {
+	showDatePicker(context: context, 
+	firstDate: DateTime(2020),
+	lastDate: DateTime(2095));
+}
