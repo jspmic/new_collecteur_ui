5311739c9750195b1e5ecba471e793f4b6db19fe
Jaspe Michael <micaeljaspe@gmail.com>
2025-07-17 22:57:39 +0200

    almost finished writing the design of this screen

--- lib/screens/superviseurs.dart
+++ lib/screens/superviseurs.dart
@@ -1,20 +1,145 @@
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:new_collecteur_ui/models/options.dart';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:new_collecteur_ui/globals.dart';
+import 'package:new_collecteur_ui/models/superviseur.dart';
+import 'package:new_collecteur_ui/api/superviseur_api.dart';
 
 class Superviseurs extends StatefulWidget {
-  final Option opt;
-  const Superviseurs({super.key,
-  					required this.opt});
+  const Superviseurs({super.key});
 
   @override
   State<Superviseurs> createState() => _SuperviseursState();
 }
 
 class _SuperviseursState extends State<Superviseurs> {
+	void apply(BuildContext context) async {
+		await showDialog(context: context,
+			builder: (context) => ContentDialog(
+				title: const Text("Confirmation"),
+				content: const Text("Appliquer les changements?"),
+				actions: [
+					Button(
+						onPressed: (){},
+						child: const Text("Oui"),
+					),
+					Button(
+						onPressed: () => Navigator.pop(context),
+						child: const Text("Non"),
+					),
+				],
+			)
+		);
+	}
+
+	void deny(BuildContext context) async {
+		await showDialog(context: context,
+			builder: (context) => ContentDialog(
+				title: const Text("Confirmation"),
+				content: const Text("Annuler les changements?"),
+				actions: [
+					Button(
+						onPressed: (){},
+						child: const Text("Oui"),
+					),
+					Button(
+						onPressed: () => Navigator.pop(context),
+						child: const Text("Non"),
+					),
+				],
+			)
+		);
+	}
+
+	void addSuperviseur(BuildContext context) async {
+		bool isLoading = false;
+		TextEditingController pssw = TextEditingController();
+		TextEditingController nom = TextEditingController();
+		Superviseur s = Superviseur(nom_utilisateur: "", id: 0);
+		await showDialog(context: context,
+			builder: (context) => ContentDialog(
+				title: const Text("Ajouter un superviseur"),
+				content: Column(
+					mainAxisAlignment: MainAxisAlignment.start,
+					mainAxisSize: MainAxisSize.min,
+					children: [
+						TextBox(
+							controller: nom,
+							placeholder: "Nom du superviseur (minuscule, sans espace)",
+						),
+						SizedBox(height: MediaQuery.of(context).size.height/25),
+						PasswordBox(
+							controller: pssw,
+							revealMode: PasswordRevealMode.peek,
+							placeholder: "Mot de passe...",
+						),
+					],
+				), // Column
+				actions: [
+					Button(
+						onPressed: () async {
+							if (pssw.text.isEmpty || nom.text.isEmpty) {
+								return;
+							}
+							String password = sha256.convert(utf8.encode(pssw.text)).toString();
+							s.nom_utilisateur = nom.text;
+							s.psswd = password;
+							bool status = await addSuperviseurs(s);
+							if (status) {
+							}
+							else {
+							}
+						},
+						child: isLoading ? ProgressRing() : Text("Ajouter"),
+					),
+					Button(
+						onPressed: () => Navigator.pop(context),
+						child: const Text("Annuler"),
+					),
+				],
+			)
+		);
+		setState(() { });
+	}
 	@override
 	  Widget build(BuildContext context) {
 	  	return ScaffoldPage(
-			header: PageHeader(title: Center(child: Text(widget.opt.title))),
+			header: PageHeader(
+			title: Text("Superviseurs"),
+			commandBar: CommandBar(primaryItems: [
+				CommandBarButton(
+					onPressed: () => addSuperviseur(context),
+					label: Text("Ajouter un superviseur"),
+					icon: Icon(FluentIcons.add)
+				),
+				CommandBarButton(
+					onPressed: () => apply(context),
+					label: Text("Appliquer"),
+					tooltip: "Appliquer les changements",
+					icon: Icon(FluentIcons.accept)
+				),
+				CommandBarButton(
+					onPressed: () => deny(context),
+					label: Text("Annuler"),
+					tooltip: "Annuler les changements",
+					icon: Icon(FluentIcons.calculator_multiply)
+				)
+			]),
+			), // PageHeader
+			content: ListView.builder(
+				shrinkWrap: true,
+				itemCount: superviseursList.length,
+				itemBuilder: (context, index) {
+					final superviseur = superviseursList[index];
+					final pssw = superviseur.psswd;
+					return Expander(
+						leading: IconButton(onPressed: (){},
+						icon: Icon(FluentIcons.calculator_multiply)),
+						header: TextBox(placeholder: superviseur.nom_utilisateur),
+						content: PasswordBox(placeholder: pssw ?? "Nouveau mot de passe pour le superviseur...", revealMode: PasswordRevealMode.peek),
+					); // Expander
+				}
+			),
 		);
 	  }
 }
