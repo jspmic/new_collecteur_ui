c4cc20ba028fba4d22c290cdb92788a32606feb1
Jaspe Michael <micaeljaspe@gmail.com>
2025-07-17 22:57:08 +0200

    feat: removed manual insertion of the superviseursList which was intended for testing

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,11 +3,7 @@ import "package:new_collecteur_ui/models/lot.dart";
 import "package:new_collecteur_ui/models/superviseur.dart";
 
 List<String> types_mouvement = ["Livraison", "Transfert"];
-List<Superviseur> superviseursList = [
-	Superviseur(nom_utilisateur: "jaspe", id: 0),
-	Superviseur(nom_utilisateur: "ella", id: 1),
-	Superviseur(nom_utilisateur: "divin", id: 2),
-];
+List<Superviseur> superviseursList = [];
 Map<String, District> districts = {"Gihogazi": District(id: 0, nom: "Gihogazi", collines: ["Gasambura", "Gitaramuka"])};
 List<Lot> lots = [Lot(id: 0, nom: "Lot 1", districts: ["Gihogazi"]), Lot(id: 1, nom: "Lot 2", districts: ["Gihogazi"])];
 
