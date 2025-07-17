db6418ddf71af9ffb597d13d89b122397c08cd08
Jaspe Michael <micaeljaspe@gmail.com>
2025-07-17 22:56:15 +0200

    feat: added the almost-finished widgets and the essential features

--- lib/screens/districts_collines.dart
+++ lib/screens/districts_collines.dart
@@ -1,20 +1,76 @@
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:new_collecteur_ui/models/options.dart';
+import 'package:new_collecteur_ui/globals.dart';
+import 'package:new_collecteur_ui/models/lot.dart';
 
-class Mouvements extends StatefulWidget {
-  final Option opt;
-  const Mouvements({super.key,
-  					required this.opt});
+class DistrictsCollines extends StatefulWidget {
+  const DistrictsCollines({super.key});
 
   @override
-  State<Mouvements> createState() => _MouvementsState();
+  State<DistrictsCollines> createState() => _DistrictsCollinesState();
 }
 
-class _MouvementsState extends State<Mouvements> {
+class _DistrictsCollinesState extends State<DistrictsCollines> {
+	String lotSelected = "";
 	@override
 	  Widget build(BuildContext context) {
 	  	return ScaffoldPage(
-			header: PageHeader(title: Center(child: Text(widget.opt.title))),
+			header: PageHeader(title: Center(child: const Text("Districts et Collines"))),
+			content: Container(
+				child:
+					ListView.builder(
+						shrinkWrap: true,
+						itemCount: lots.length,
+						itemBuilder: (context, index) {
+							final lot = lots[index];
+							return Expander(
+								leading: Icon(FluentIcons.document),
+								trailing: const Text("Lot"),
+								header: SizedBox(
+									width: MediaQuery.of(context).size.width/4,
+									child: 
+										TextBox(
+											placeholder: lotSelected == "" ? lot.nom : lotSelected,
+											onChanged: (value){
+												Lot copyLot = lot;
+												copyLot.nom = value;
+												lotsChanged[index] = copyLot;
+												if (lotsChanged[index]!.nom.isEmpty) {
+													lotsChanged.remove(index);
+												}
+												print("$lotsChanged");
+											},
+										)
+								),
+								content: ListView.builder(
+									shrinkWrap: true,
+									itemCount: lot.districts.length,
+									itemBuilder: (context, index2) {
+										final district = lot.districts[index2];
+										return SingleChildScrollView(child: Expander(
+											leading: Icon(FluentIcons.edit_mirrored),
+											trailing: const Text("District"),
+											header: SizedBox(
+												child: TextBox(placeholder: district)
+											),
+											content: ListView.builder(
+												shrinkWrap: true,
+												itemCount: districts[district]!.collines.length,
+												itemBuilder: (context, index3) {
+													final colline = districts[district]!.collines[index3];
+													return TextBox(placeholder: colline);
+												},
+											), // builder-content
+										));
+									},
+								), // builder-content
+								onStateChanged: (open) {
+									lotSelected = lot.nom;
+									print(lotSelected);
+								}
+							); // Expander
+						}
+					), // ListView
+			) // Center
 		);
 	  }
 }
