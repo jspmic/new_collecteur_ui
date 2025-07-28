b6edbca2c04af7cdf1fe62a21f22a759cc7406bc
Jaspe Michael <micaeljaspe@gmail.com>
2025-07-28 20:34:19 +0200

    feat: removed id from District since it is incoherent with the API

--- lib/models/district.dart
+++ lib/models/district.dart
@@ -5,22 +5,18 @@ District districtFromJson(String str) => District.fromJson(json.decode(str));
 String districtToJson(District data) => json.encode(data.toJson());
 
 class District {
-	int id;
 	String nom;
-	List<String> collines;
+	List collines;
 	District({
-		required this.id,
 		required this.nom,
 		required this.collines
 	});
     factory District.fromJson(Map<String, dynamic> json) => District(
-        id: json["id"],
         nom: json["nom"],
         collines: json["collines"],
     );
 
     Map<String, dynamic> toJson() => {
-        "id": id,
         "nom": nom,
         "collines": collines,
     };
