e19bdb159356ce6fbcd17583e6a4249727aa44a6
Jaspe Michael <micaeljaspe@gmail.com>
2025-07-28 20:32:56 +0200

    feat: added skeleton for this screen

--- lib/screens/autres_champs.dart
+++ lib/screens/autres_champs.dart
@@ -1,5 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:new_collecteur_ui/models/options.dart';
+import 'package:new_collecteur_ui/globals.dart';
 
 class AutresChamps extends StatefulWidget {
   final Option opt;
@@ -11,10 +12,33 @@ class AutresChamps extends StatefulWidget {
 }
 
 class _AutresChampsState extends State<AutresChamps> {
+	final Set<int> expandedLots = {};
 	@override
 	  Widget build(BuildContext context) {
 	  	return ScaffoldPage(
 			header: PageHeader(title: Center(child: Text(widget.opt.title))),
+			content: Column(
+				children: [
+					Expander(
+						header: Text("Produits"),
+						content: Text(""),
+					),
+					SizedBox(
+						height: MediaQuery.of(context).size.height/30,
+					), // SizedBox
+					Expander(
+						header: Text("Stocks"),
+						content: Text(""),
+					),
+					SizedBox(
+						height: MediaQuery.of(context).size.height/30,
+					), // SizedBox
+					Expander(
+						header: Text("Type de transports"),
+						content: Text(""),
+					),
+				],
+			),
 		);
 	  }
 }
