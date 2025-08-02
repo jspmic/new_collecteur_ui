68f806616cc02e7c8dae5e3264c64368865fe2ac
Jaspe Michael <micaeljaspe@gmail.com>
2025-08-02 12:36:16 +0200

    fix: modified initialization functions and added functions to save changes

--- lib/custom_widgets.dart
+++ lib/custom_widgets.dart
@@ -2,6 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:new_collecteur_ui/models/superviseur.dart';
 import 'package:new_collecteur_ui/api/superviseur_api.dart';
 import 'package:new_collecteur_ui/globals.dart';
+import 'package:new_collecteur_ui/api/autres_champs_api.dart';
 
 void clearGlobals() {
 	superviseursList = [];
@@ -10,14 +11,33 @@ void clearGlobals() {
 }
 
 void initializeGlobals() {
-	superviseursList = [Superviseur(id: 0, nom_utilisateur: "")];
+	superviseursList = [Superviseur(id: 0, nom_utilisateur: "", nom: "", lot: "")];
 }
+
+// This function saves a certain movement to the new content
+// void saveModified(String movement, String id, Map<String, String> content, {int? boucleId}){
+// 	//   if (movement == "Livraison"){
+// 	// modifiedLivraisons.containsKey(id) ? modifiedLivraisons[id]!.addAll(content)
+// 	// : modifiedLivraisons[id] = content;
+// 	//   }
+// 	//   else {
+// 	modifiedTransferts.containsKey(id) ? modifiedTransferts[id]!.addAll(content)
+// 	: modifiedTransferts[id] = content;
+//   // }
+// }
+
+
+// void saveChange(String movement, {required int id,
+// 				required String columnName, required String newValue}){
+//   saveModified(movement, id.toString(), {columnName: newValue});
+// }
 
 Future<bool> init() async {
 	await dotenv.load(fileName: ".env");
 	clearGlobals();
 	try {
 		bool loadingSuperviseurs = await getSuperviseurs();
+		bool loadingFields = await getFields();
 		if (!loadingSuperviseurs) {
 			return false;
 		}
@@ -28,9 +48,9 @@ Future<bool> init() async {
 }
 
 // Function to format a date
-String? formatDate(DateTime? _date){
+String formatDate(DateTime? _date){
   if (_date == null){
-    return null;
+    return "";
   }
-  return "${_date?.day} / ${_date?.month} / ${_date?.year}";
+  return "${_date.day}/${_date.month}/${_date.year}";
 }
