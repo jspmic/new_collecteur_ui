0d7d7b77e259ceac086b7b9e5a42275948f2097e
jspmic <micaeljaspe@gmail.com>
2025-12-29 00:42:42 +0200

    feat: finished adding dynamic display of the 'autres champs' screen

--- lib/screens/autres_champs.dart
+++ lib/screens/autres_champs.dart
@@ -1,6 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:new_collecteur_ui/models/options.dart';
 import 'package:new_collecteur_ui/globals.dart';
+import 'package:new_collecteur_ui/custom_widgets.dart';
 
 class AutresChamps extends StatefulWidget {
   final Option opt;
@@ -12,33 +13,82 @@ class AutresChamps extends StatefulWidget {
 }
 
 class _AutresChampsState extends State<AutresChamps> {
-	final Set<int> expandedLots = {};
+	final Set<int> expandedInputs = {};
 	@override
 	  Widget build(BuildContext context) {
 	  	return ScaffoldPage(
 			header: PageHeader(title: Center(child: Text(widget.opt.title))),
-			content: Column(
+			content: SingleChildScrollView(
+			child: Column(
 				children: [
 					Expander(
 						header: Text("Produits"),
-						content: Text(""),
+						content: SizedBox(
+							height: MediaQuery.of(context).size.height/3,
+						child: ListView.builder(
+						itemCount: collectedInputs.length,
+						itemBuilder: (context, inputIndex) {
+							return Padding(
+								padding: const EdgeInsets.only(left: 20),
+								child: Column(
+									children: [
+										ListTile(
+											title: Text(collectedInputs[inputIndex].toString()),
+										)
+									],
+								)
+							);
+						})
+						)
 					),
 					SizedBox(
 						height: MediaQuery.of(context).size.height/30,
 					), // SizedBox
 					Expander(
 						header: Text("Stocks"),
-						content: Text(""),
+							content: SizedBox(
+									height: MediaQuery.of(context).size.height/3,
+									child: ListView.builder(
+											itemCount: collectedStocks.length,
+											itemBuilder: (context, inputIndex) {
+												return Padding(
+														padding: const EdgeInsets.only(left: 20),
+														child: Column(
+															children: [
+																ListTile(
+																	title: Text(formatStock(collectedStocks[inputIndex].toString())),
+																)
+															],
+														)
+												);
+											})
+							)
 					),
 					SizedBox(
 						height: MediaQuery.of(context).size.height/30,
 					), // SizedBox
 					Expander(
 						header: Text("Type de transports"),
-						content: Text(""),
+							content: SizedBox(
+									height: MediaQuery.of(context).size.height/3,
+									child: ListView.builder(
+											itemCount: collectedTypeTransports.length,
+											itemBuilder: (context, inputIndex) {
+												return Padding(
+														padding: const EdgeInsets.only(left: 20),
+														child: Column(
+															children: [
+																ListTile(
+																	title: Text(collectedTypeTransports[inputIndex].toString()),
+																)
+															],
+														)
+												);
+											})
+							)
 					),
 				],
 			),
-		);
+			));
 	  }
 }
